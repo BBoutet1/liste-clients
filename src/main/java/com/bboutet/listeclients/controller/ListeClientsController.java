4f513fce6a0bce15f113e56ca20f0fb5b3d4ae52
Brice Boutet <boutetlb@gmail.com>
2020-12-20 14:16:34 -0500

    Controller and model completed

---
+++ src/main/java/com/bboutet/listeclients/controller/ListeClientsController.java
@@ -0,0 +1,53 @@
+package com.bboutet.listeclients.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import antlr.collections.List;
+import ch.qos.logback.core.net.server.Client;
+
+//Controller
+@RestController
+@RequestMapping("/api")
+public class ListeClientsController {
+   @Autowired
+	private clientService cliensservice;
+	
+	@GetMapping("/clients")
+	public List<Client> get() {
+		return clientService.get();
+	}
+	
+	@PostMapping("/client")
+	public Client save(@RequestBody Employee employee) {
+		employeeService.save(employee);
+		return employee;
+	}
+	
+	@GetMapping("/employee/{id}")
+	public Employee get(@PathVariable int id) {
+		return employeeService.get(id);
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public String delete(@PathVariable int id) {
+		
+		employeeService.delete(id);
+		
+		return "Employee removed with id "+id;
+		
+	}
+	
+	@PutMapping("/employee")
+	public Employee update(@RequestBody Employee employee) {
+		employeeService.save(employee);
+		return employee;
+	}
+    
+}
---
+++ src/main/java/com/bboutet/listeclients/model/Client.java
@@ -0,0 +1,65 @@
+package com.bboutet.listeclients.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "table_client")
+public class Client {
+    	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+
+	@Column
+	private String lastName;
+
+	@Column
+	private String firstName;
+
+	@Column
+	private Integer phone;
+
+	@Override
+	public String toString() {
+		return "Client [ID=	" + id + ", NOM=" + lastName + ", PRENOM=" + firstName + ", TELEPHONE=" + phone +
+				 "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public void getFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+}
