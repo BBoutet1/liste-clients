37a68ccd7fefb9a72aa2d53b847a2936ccfd28d0
Brice Boutet <boutetlb@gmail.com>
2020-12-19 22:06:36 -0500

    first commit

---
+++ src/main/java/com/bboutet/listeclients/ListeClientsApplication.java
@@ -0,0 +1,13 @@
+package com.bboutet.listeclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bboutet/listeclients/ListeClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bboutet.listeclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListeClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
