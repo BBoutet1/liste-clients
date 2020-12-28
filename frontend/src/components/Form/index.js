320ec2478e34ee8b52844d0f49530b05c6ddc684
Brice Boutet <boutetlb@gmail.com>
2020-12-28 02:04:42 -0500

    Form inputs retrieved using react useState

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -3,17 +3,24 @@ import "./index.css"
 
 export default function Form(){
   // Setting our component's initial state
-  const [client, setClient] = useState({});
+    const [client, setClient] = useState({});
+
+    
 
   // Load all books and store them with setBooks
 //   useEffect(() => {
 //     loadUserData();
 //   }, []);
 
+    const handleInputChange = event => { 
+        event.preventDefault();
+       const { name, value } = event.target;
+       if (event.target.value){setClient({...client, [name]: value})}
+        console.log(client)
+    }
  
-    const handleSubmit = (e) => {
-        let data = e.event;
-        console.log(data)
+    const handleSubmit = event => {
+     
 //   API.create()
 //       .then(res => {
 //         this.setState({
@@ -25,18 +32,24 @@ export default function Form(){
     return (
         
 <form onSubmit={handleSubmit}>
-  <div class="form-row mb-2 mt-1">
+  <div class="form-row mb-2 mt-1 mb-0">
     <div class="col mt-1">
-      <input type="text" class="form-control" placeholder="Nom"/>
+        <input type="text" id="lastname" name="lastname" class="form-control" placeholder="Nom"
+            onChange={handleInputChange}
+        />
     </div>
     <div class="col mt-1">
-      <input type="text" class="form-control" placeholder="Prénom"/>
+        <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Prénom"
+            onChange={handleInputChange}
+        />
     </div>
      <div class="col mt-1">
-      <input type="tel" class="form-control" placeholder="Téléphone" pattern="[(][0-9]{3}[)] [0-9]{3}-[0-9]{4}" />
-    </div>  
-    <button type="submit" class="btn btn-primary pr-2 pl-2  mt-1 mr-1 ml-1">Sauvergarder</button>            
- </div>            
+        <input type="tel" id="phone" name="phone" class="form-control mr-2" placeholder="Téléphone"
+            onChange={handleInputChange}
+        />
+    </div>            
+   </div>
+   <button type="submit" class="btn btn-primary pr-2 pl-2  mt-1 mr-1">Sauvergarder</button>           
 </form>)
 
 }
